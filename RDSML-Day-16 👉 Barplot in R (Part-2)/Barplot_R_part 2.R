e5c76f3ed86eb8a95922be1572be05366c02af32
Farzanasid1 <farzanasiddika.bracu@gmail.com>
2025-06-26 02:43:12 +1000

    barplot_2

---
+++ RDSML-Day-16 👉 Barplot in R (Part-2)/Barplot_R_part 2.R
@@ -0,0 +1,55 @@
+# Horizontal barplotMore actions
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+# Changing the width of bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", width = .5)
+
+# Changing colors
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", color = "blue", fill = "white", width = .5)
+
+# Minimal theme with blue fill color
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  theme_minimal()
+
+# Barplot with labels
+# Outeside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = -0.5, size = 3, color = "#9F9F9F") +
+  theme_minimal()
+
+# Inside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = 1.6, size = 3, color = "white") +
+  theme_minimal()
+
+myCarData = mtcars
+# Barplot of counts
+# To make a barplot of counts, we will use mtcars dataset
+
+ggplot(data = myCarData, aes(x = factor(cyl))) +
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") +
+  theme_minimal()
+dataset = data.frame(dose = c("D0.5", "D1", "D2"), More actions
+                     len = c(4.2, 10, 29.5))
+library(ggplot2)
+dataset = data.frame(dose = c("D0.5", "D1", "D2"),
+                     len = c(4.2, 10, 29.5))
+#DAY16
+#Changing barplot line colors by 
+print(dataset)
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(start = "identity", fill = "White") +
+  scale_color_manual(values = c("blue", "red", "green"))
+
+
+# Using black outline color
+ggplot(dataset, aes(x = dose, y = len, fill = dose)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_color_manual(values = c("#85713a", "#c72ce6", "#f00a24"))
